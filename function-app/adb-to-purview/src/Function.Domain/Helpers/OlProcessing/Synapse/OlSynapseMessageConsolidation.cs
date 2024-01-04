be7935679e305883f2dbc86481529459c6c4078f
Ch Mani Kant <chkant@microsoft.com>
2024-01-04 14:11:02 +0530

    Code changes : Reducing the function size added two povate methods : CaptureMessageAsync and ConsolidateEventsAsync

--- function-app/adb-to-purview/src/Function.Domain/Helpers/OlProcessing/Synapse/OlSynapseMessageConsolidation.cs
+++ function-app/adb-to-purview/src/Function.Domain/Helpers/OlProcessing/Synapse/OlSynapseMessageConsolidation.cs
@@ -26,99 +26,109 @@ namespace Function.Domain.Helpers
         {
             try
             {
-                string prefixInput = runId + "/Input/";
-                string prefixOutput = runId + "/Output/";
-
-                List<Task> inputUploadTasks = new List<Task>();
-                List<Task> outputUploadTasks = new List<Task>();
-
-                //Assumption: We assume the number of input / outputs is small so parallelism is currently unbounded.
-                //TODO : Implement bounded parallel calls using batch/ chunk if there are huge input/outputs
-
-                // Parallelize input uploads
-                inputUploadTasks.AddRange(olEvent.Inputs.Select(async (item) =>
-                {
-                    var inputJson = JsonConvert.SerializeObject(item);
-                    string inputBlobName = prefixInput + GetUniqueHash(item.Name, item.NameSpace);
-                    // Check if the blob already exists
-                    if (!await _blobClientFactory.ExistsAsync(CONTAINER_NAME, inputBlobName))
-                    {
-                        return _blobClientFactory.UploadAsync(CONTAINER_NAME, inputBlobName, inputJson);
-                    }
-                    // If it already exists, return a completed task
-                    return Task.CompletedTask;
-                }));
-
-                // Parallelize output uploads
-                outputUploadTasks.AddRange(olEvent.Outputs.Select(async (item) =>
-                {
-                    var outputJson = JsonConvert.SerializeObject(item);
-                    string outputBlobName = prefixOutput + GetUniqueHash(item.Name, item.NameSpace);
-                    // Check if the blob already exists
-                    if (!await _blobClientFactory.ExistsAsync(CONTAINER_NAME, outputBlobName))
-                    {
-                        return _blobClientFactory.UploadAsync(CONTAINER_NAME, outputBlobName, outputJson);
-                    }
-
-                    // If it already exists, return a completed task
-                    return Task.CompletedTask;
-                }));
-
-                // Await all upload tasks
-                await Task.WhenAll(inputUploadTasks.Concat(outputUploadTasks));
-
-
-                // Get list of input and outputs which are stored in blob
-                List<string> inputs = await _blobClientFactory.GetBlobsByHierarchyAsync(prefixInput);
-                List<string> outputs = await _blobClientFactory.GetBlobsByHierarchyAsync(prefixOutput);
-
-                // If there is at least 1 input and 1 output - proceed for message consolidation
-                if (inputs.Count > 0 && outputs.Count > 0)
-                {
-                    List<Inputs?> inputsList = new List<Inputs?>();
-                    List<Outputs?> outputsList = new List<Outputs?>();
-
-                    // Get all the inputs
-                    List<Task<Inputs?>> inputTasks = inputs.Select(async item =>
-                    {
-                        var inputObject = await _blobClientFactory.DownloadBlobAsync(CONTAINER_NAME, item);
-                        return JsonConvert.DeserializeObject<Inputs?>(inputObject);
-                    }).ToList();
-
-                    // Get all the outputs
-                    List<Task<Outputs?>> outputTasks = outputs.Select(async item =>
-                    {
-                        var outputObject = await _blobClientFactory.DownloadBlobAsync(CONTAINER_NAME, item);
-                        return JsonConvert.DeserializeObject<Outputs?>(outputObject);
-                    }).ToList();
-
-                    inputsList = (await Task.WhenAll(inputTasks)).Where(input => input != null).ToList();
-                    outputsList = (await Task.WhenAll(outputTasks)).Where(output => output != null).ToList();
-
-                    // Message Consolidation - Update inputs / outputs to the current olevent object
-                    if (inputsList.Count > 0)
-                    {
-                        olEvent.Inputs.Clear();
-                        olEvent.Inputs.AddRange(inputsList);
-                    }
-
-                    // to do change
-                    if (outputsList.Count > 0)
-                    {
-                        olEvent.Outputs.Clear();
-                        olEvent.Outputs.AddRange(outputsList);
-                    }
-                    return olEvent;
-                }
-                else
-                {
-                    return null;
-                }
+                var result = await CaptureMessageAsync(olEvent, runId);
+                return await ConsolidateEventsAsync(olEvent, result);
             }
             catch (Exception ex)
             {
                 _log.LogError(ex, $"OlSynapseMessageConsolidation-ConsolidateEventAsync: Error {ex.InnerException} ", ex.Message);
                 throw;
+            }
+        }
+
+        private async Task<(List<string> Inputs, List<string> Outputs)> CaptureMessageAsync(Event olEvent, string runId)
+        {
+            string prefixInput = runId + "/Input/";
+            string prefixOutput = runId + "/Output/";
+
+            List<Task> inputUploadTasks = new List<Task>();
+            List<Task> outputUploadTasks = new List<Task>();
+
+            //Assumption: We assume the number of input / outputs is small so parallelism is currently unbounded.
+            //TODO : Implement bounded parallel calls using batch/ chunk if there are huge input/outputs
+
+            // Parallelize input uploads
+            inputUploadTasks.AddRange(olEvent.Inputs.Select(async (item) =>
+            {
+                var inputJson = JsonConvert.SerializeObject(item);
+                string inputBlobName = prefixInput + GetUniqueHash(item.Name, item.NameSpace);
+                // Check if the blob already exists
+                if (!await _blobClientFactory.ExistsAsync(CONTAINER_NAME, inputBlobName))
+                {
+                    return _blobClientFactory.UploadAsync(CONTAINER_NAME, inputBlobName, inputJson);
+                }
+                // If it already exists, return a completed task
+                return Task.CompletedTask;
+            }));
+
+            // Parallelize output uploads
+            outputUploadTasks.AddRange(olEvent.Outputs.Select(async (item) =>
+            {
+                var outputJson = JsonConvert.SerializeObject(item);
+                string outputBlobName = prefixOutput + GetUniqueHash(item.Name, item.NameSpace);
+                // Check if the blob already exists
+                if (!await _blobClientFactory.ExistsAsync(CONTAINER_NAME, outputBlobName))
+                {
+                    return _blobClientFactory.UploadAsync(CONTAINER_NAME, outputBlobName, outputJson);
+                }
+
+                // If it already exists, return a completed task
+                return Task.CompletedTask;
+            }));
+
+            // Await all upload tasks
+            await Task.WhenAll(inputUploadTasks.Concat(outputUploadTasks));
+
+
+            // Get list of input and outputs which are stored in blob
+            List<string> inputs = await _blobClientFactory.GetBlobsByHierarchyAsync(prefixInput);
+            List<string> outputs = await _blobClientFactory.GetBlobsByHierarchyAsync(prefixOutput);
+            return (inputs, outputs);
+        }
+
+        private async Task<Event?> ConsolidateEventsAsync(Event olEvent, (List<string> Inputs, List<string> Outputs) result)
+        {
+            // If there is at least 1 input and 1 output - proceed for message consolidation
+            if (result.Inputs != null && result.Inputs.Count > 0 && result.Outputs != null && result.Outputs.Count > 0)
+            {
+                List<Inputs?> inputsList = new List<Inputs?>();
+                List<Outputs?> outputsList = new List<Outputs?>();
+
+                // Get all the inputs
+                List<Task<Inputs?>> inputTasks = result.Inputs.Select(async item =>
+                {
+                    var inputObject = await _blobClientFactory.DownloadBlobAsync(CONTAINER_NAME, item);
+                    return JsonConvert.DeserializeObject<Inputs?>(inputObject);
+                }).ToList();
+
+                // Get all the outputs
+                List<Task<Outputs?>> outputTasks = result.Outputs.Select(async item =>
+                {
+                    var outputObject = await _blobClientFactory.DownloadBlobAsync(CONTAINER_NAME, item);
+                    return JsonConvert.DeserializeObject<Outputs?>(outputObject);
+                }).ToList();
+
+                inputsList = (await Task.WhenAll(inputTasks)).Where(input => input != null).ToList();
+                outputsList = (await Task.WhenAll(outputTasks)).Where(output => output != null).ToList();
+
+                // Message Consolidation - Update inputs / outputs to the current olevent object
+                if (inputsList.Count > 0)
+                {
+                    olEvent.Inputs.Clear();
+                    olEvent.Inputs.AddRange(inputsList);
+                }
+
+                // to do change
+                if (outputsList.Count > 0)
+                {
+                    olEvent.Outputs.Clear();
+                    olEvent.Outputs.AddRange(outputsList);
+                }
+                return olEvent;
+            }
+            else
+            {
+                return null;
             }
         }
 
