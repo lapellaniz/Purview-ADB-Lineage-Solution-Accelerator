4f3fbf4e9691c514654cc94eb9ee2b978093f904
Ch Mani Kant <chkant@microsoft.com>
2024-01-24 19:26:12 +0530

    Adding the warning code

--- function-app/adb-to-purview/src/Function.Domain/Helpers/Logging/ErrorCodes.cs
+++ function-app/adb-to-purview/src/Function.Domain/Helpers/Logging/ErrorCodes.cs
@@ -5,7 +5,7 @@ namespace Function.Domain.Helpers.Logging
 
         public static class OpenLineage
         {
-            public const int GenericError = 100;  //100-199
+            public const int GenericError = 100;
             public const int EventValidation = 101;
             public const int JsonSerialization = 102;
             public const int ParseEvent = 103;
@@ -13,7 +13,7 @@ namespace Function.Domain.Helpers.Logging
 
         public static class PurviewOut
         {
-            public const int GenericError = 200; //200-299
+            public const int GenericError = 200;
             public const int SynapseOlMessageGeneric = 201;
             public const int OlSynapseMessageEnrichment = 202;
             public const int OlSynapseMessageConsolidation = 203;
@@ -28,9 +28,8 @@ namespace Function.Domain.Helpers.Logging
         }
         public static class Warnings
         {
-            // 300 +
-            public const int OlSynapseMessageEnrichmentCaptureNameSpace = 300;   //Generic warning codes
-            public const int Warning2 = 301;   // New unexpected data assets in Purview  TODO Mani Sendtopurview
+            public const int OlSynapseMessageEnrichmentCaptureNameSpace = 300;
+            public const int OlSynapseUnexpectedData = 301;
         }
     }
 }
